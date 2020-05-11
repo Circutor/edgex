c4f9fa35ae9615de0460a8b2e94c9bc13a884cb1
Joan Duran <jduran@circutor.com>
2020-05-11 13:07:04 +0200

    S'ha afegit el camp certificate a l'addressable Ara en la lectura dels exports no apareixen els camps buits

--- pkg/models/registration.go
+++ pkg/models/registration.go
@@ -15,6 +15,7 @@
 package models
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -58,18 +59,68 @@ const (
 // Registration - Defines the registration details
 // on the part of north side export clients
 type Registration struct {
-	ID          string            `json:"id,omitempty"`
+	ID          string            `json:"id"`
 	Created     int64             `json:"created"`
 	Modified    int64             `json:"modified"`
 	Origin      int64             `json:"origin"`
-	Name        string            `json:"name,omitempty"`
-	Addressable Addressable       `json:"addressable,omitempty"`
-	Format      string            `json:"format,omitempty"`
-	Filter      Filter            `json:"filter.go,omitempty"`
-	Encryption  EncryptionDetails `json:"encryption,omitempty"`
-	Compression string            `json:"compression,omitempty"`
+	Name        string            `json:"name"`
+	Addressable Addressable       `json:"addressable"`
+	Format      string            `json:"format"`
+	Filter      Filter            `json:"filter"`
+	Encryption  EncryptionDetails `json:"encryption"`
+	Compression string            `json:"compression"`
 	Enable      bool              `json:"enable"`
-	Destination string            `json:"destination,omitempty"`
+	Destination string            `json:"destination"`
+}
+
+// Custom marshaling for JSON
+// Treat the strings as pointers so they can be null in JSON
+func (r Registration) MarshalJSON() ([]byte, error) {
+	aux := struct {
+		ID          *string            `json:"id,omitempty"`
+		Created     int64              `json:"created,omitempty"`
+		Modified    int64              `json:"modified,omitempty"`
+		Origin      int64              `json:"origin,omitempty"`
+		Name        *string            `json:"name,omitempty"`
+		Addressable Addressable        `json:"addressable"`
+		Format      *string            `json:"format,omitempty"`
+		Filter      *Filter            `json:"filter,omitempty"`
+		Encryption  *EncryptionDetails `json:"encryption,omitempty"`
+		Compression *string            `json:"compression,omitempty"`
+		Enable      bool               `json:"enable"`
+		Destination *string            `json:"destination,omitempty"`
+	}{
+		Created:     r.Created,
+		Modified:    r.Modified,
+		Origin:      r.Origin,
+		Addressable: r.Addressable,
+		Enable:      r.Enable,
+	}
+
+	// Only initialize the non-empty strings (empty are null)
+	if r.ID != "" {
+		aux.ID = &r.ID
+	}
+	if r.Name != "" {
+		aux.Name = &r.Name
+	}
+	if r.Format != "" {
+		aux.Format = &r.Format
+	}
+	if len(r.Filter.DeviceIDs) != 0 || len(r.Filter.ValueDescriptorIDs) != 0 {
+		aux.Filter = &r.Filter
+	}
+	if r.Encryption.Algo != "" || r.Encryption.Key != "" || r.Encryption.InitVector != "" {
+		aux.Encryption = &r.Encryption
+	}
+	if r.Compression != "" {
+		aux.Compression = &r.Compression
+	}
+	if r.Destination != "" {
+		aux.Destination = &r.Destination
+	}
+
+	return json.Marshal(aux)
 }
 
 func (reg Registration) Validate() (bool, error) {
