e58a3184c6be8bf50635638029f7c9d854158516
xgarcia <email@example.com>
2020-05-19 13:34:03 +0200

    comments added to crypto functs

--- internal/export/client/encrypt.go
+++ internal/export/client/encrypt.go
@@ -40,6 +40,8 @@ func Encrypt(text string) (cryptoText string, err error) {
 		return
 	}
 
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
@@ -49,6 +51,7 @@ func Encrypt(text string) (cryptoText string, err error) {
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
 
+	// convert to base64
 	cryptoText = base64.URLEncoding.EncodeToString(ciphertext)
 	return
 }
@@ -67,6 +70,8 @@ func Decrypt(cryptoText string) (text string, err error) {
 		return
 	}
 
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
 	if len(ciphertext) < aes.BlockSize {
 		err = errors.New("ciphertext too short")
 		return
@@ -76,6 +81,7 @@ func Decrypt(cryptoText string) (text string, err error) {
 
 	stream := cipher.NewCFBDecrypter(block, iv)
 
+	// XORKeyStream can work in-place if the two arguments are the same.
 	stream.XORKeyStream(ciphertext, ciphertext)
 	text = fmt.Sprintf("%s", ciphertext)
 	return
