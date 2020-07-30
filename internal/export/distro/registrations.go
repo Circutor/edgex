15a505c3e16a24880a8e5d1366ed9fccec3bb6f9
cgerman <cgerman@dezac.com>
2020-07-30 14:10:26 +0200

    Last change to avoid unexpected behaviour when Configuration.Writable.MarkPushed is false

--- internal/export/distro/registrations.go
+++ internal/export/distro/registrations.go
@@ -203,11 +203,8 @@ func (reg registrationInfo) processEvent(event *models.Event) {
 }
 
 func registrationLoop(reg *registrationInfo) {
-	var timerPush *time.Timer
 	LoggingClient.Info(fmt.Sprintf("registration loop started: %s", reg.registration.Name))
-	if Configuration.Writable.MarkPushed {
-		timerPush = time.NewTimer(pushEventsTimer * time.Second)
-	}
+	timerPush := time.NewTimer(pushEventsTimer * time.Second)
 	for {
 		select {
 		case event := <-reg.chEvent:
@@ -230,22 +227,24 @@ func registrationLoop(reg *registrationInfo) {
 				}
 			}
 		case <-timerPush.C:
-			events, err := ec.Events(context.Background())
-			if err != nil {
-				LoggingClient.Error(fmt.Sprintf("Failed getting events to send non-pushed %s", err.Error()))
-			}
-			LoggingClient.Info("Pushing unpushed events")
-			nPushed := 0
-			for i := range events {
-				if events[i].Pushed == 0 {
-					LoggingClient.Info("Unpushed event found, pushing...")
-					correlationID := uuid.New()
-					ev := models.Event{CorrelationId: correlationID.String(), Event: events[i]}
-					reg.processEvent(&ev)
-					nPushed++
+			if Configuration.Writable.MarkPushed {
+				events, err := ec.Events(context.Background())
+				if err != nil {
+					LoggingClient.Error(fmt.Sprintf("Failed getting events to send non-pushed %s", err.Error()))
 				}
-				if nPushed >= 100 {
-					break
+				LoggingClient.Info("Pushing unpushed events")
+				nPushed := 0
+				for i := range events {
+					if events[i].Pushed == 0 {
+						LoggingClient.Info("Unpushed event found, pushing...")
+						correlationID := uuid.New()
+						ev := models.Event{CorrelationId: correlationID.String(), Event: events[i]}
+						reg.processEvent(&ev)
+						nPushed++
+					}
+					if nPushed >= 100 {
+						break
+					}
 				}
 			}
 			timerPush.Reset(pushEventsTimer * time.Second)
